71398988b4a9f029c166a142fb284f4571424cc5
ALLOWAKOU Ferdinand <allowakouferdinand@gmail.com>
2022-10-21 14:54:30 +0100

    maj

--- drones/serializers.py
+++ drones/serializers.py
@@ -46,3 +46,39 @@ class CompetitionSerializer(serializers.HyperlinkedModelSerializer):
         'distance_in_feet',
         'distance_achievement_date',
         'drone')
+
+class PilotSerializer(serializers.HyperlinkedModelSerializer):
+    competitions = CompetitionSerializer(many=True, read_only=True)
+    gender = serializers.ChoiceField(
+    choices=Pilot.GENDER_CHOICES)
+    gender_description = serializers.CharField(
+    source='get_gender_display',
+    read_only=True)
+    
+    class Meta:
+        model = Pilot
+        fields = (
+        'url',
+        'name',
+        'gender',
+        'gender_description',
+        'races_count',
+        'inserted_timestamp',
+        'competitions')
+
+
+class PilotCompetitionSerializer(serializers.ModelSerializer):
+    # Display the pilot's name
+    pilot = serializers.SlugRelatedField(queryset=Pilot.objects.all(),slug_field='name')
+    # Display the drone's name
+    drone = serializers.SlugRelatedField(queryset=Drone.objects.all(),
+    slug_field='name')
+    class Meta:
+        model = Competition
+        fields = (
+        'url',
+        'pk',
+        'distance_in_feet',
+        'distance_achievement_date',
+        'pilot',
+        'drone')
